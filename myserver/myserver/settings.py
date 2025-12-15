4b72934d6e51829fe4b5705774223ba457b75b8f
Blessing C Nwobodo <nwobodoblessingc@gmail.com>
2025-12-15 15:18:12 +0100

    edited the settings.py

--- myserver/myserver/settings.py
+++ myserver/myserver/settings.py
@@ -116,3 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Add this section to silence the models.W042 warning
+# Recommended by Django for modern projects.
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
